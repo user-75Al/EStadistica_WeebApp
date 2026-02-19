6472fdeae1d29020fbf2ea7b02a59155b97d3008
Ales <alessandroburgos02.com>
2026-02-19 12:41:52 -0600

    Configurar despliegue en GitHub Pages y HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Layout from './presentation/componentes/Layout';
 import HomePage from './presentation/paginas/HomePage';
 import CalculosPage from './presentation/paginas/CalculosPage';
